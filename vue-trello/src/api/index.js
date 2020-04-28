1a317423f55ea7b1f237ff29ceb65b33ab69373b
류길수 <>
2020-04-29 00:36:22 +0900

    로그인 연동

--- vue-trello/src/api/index.js
+++ vue-trello/src/api/index.js
@@ -16,13 +16,24 @@ const request = (method, url, data) => {
         .then(result => result.data)
         .catch(result => {
             const {status} = result.response
-            if(status === UNAUTHORIZED) return onUnauthorized()
-            throw Error(result)
+            if(status === UNAUTHORIZED) onUnauthorized()
+            throw result.response
         })
+}
+
+
+export const setAuthInHeader = token => {
+   axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
 }
 
 export const board = {
     fetch() {
         return request('get', '/boards')
     }
+}
+
+export const auth = {
+   login(email, password) {
+      return request('post', '/login', {email, password})
+   }
 }
