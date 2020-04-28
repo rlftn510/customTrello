31aac3f8af57f8b3da4370c7f8058659c4aabc35
류길수 <>
2020-04-28 23:54:55 +0900

    네이게이션 가드

--- vue-trello/src/router/index.js
+++ vue-trello/src/router/index.js
@@ -9,16 +9,22 @@ import Card from '../components/Card'
 
 Vue.use(VueRouter)
 
+const requireAuth = (to, from, next) => {
+   const isAuth = localStorage.getItem('token')
+   const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+   isAuth ? next() : next(loginPath)
+}
+
 const router = new VueRouter({
   mode: 'history',
   component : {
 
   },
   routes : [
-    { path: '/', component: Home},
+    { path: '/', component: Home, beforeEnter: requireAuth },
     { path: '/login', component: Login },
-    { path: `/b/:bid`, component: Board, children: [
-        { path: 'c/:cid', component: Card }
+    { path: `/b/:bid`, component: Board, beforeEnter: requireAuth, children: [
+        { path: 'c/:cid', component: Card, beforeEnter: requireAuth }
     ]},
     { path: '*', component : NotFound},
   ]
