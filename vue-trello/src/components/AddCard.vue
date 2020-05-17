09209e4c311f40ae71e34a14f1a3ec9a4f9b1346
류길수 <>
2020-05-17 20:31:07 +0900

    카드간 이동 구현

--- vue-trello/src/components/AddCard.vue
+++ vue-trello/src/components/AddCard.vue
@@ -32,12 +32,20 @@ export default {
          if(!this.inputTitle) return
          console.log('223')
          const {inputTitle, listId} = this
+         const pos = this.newCardPos()
          this.ADD_CARD({
             title: inputTitle,
             listId
          }).finally(() => {
             this.inputTitle = ''
          })
+      },
+      newCardPos() {
+         const curList = this.$store.state.board.lists.filter(l => l.id === this.listId)[0]
+         if (!curList) return 65535
+         const {cards} = curList
+         if (!cards.length) return 65535
+         return cards[cards.length - 1].pos * 2
       },
       setupClickOutside(el) {
          document.querySelector('body').addEventListener('click', (e) => {
